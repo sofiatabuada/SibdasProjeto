7281f88815df2e58074baa193851e91fb371567d
Sofia Tabuada <1221408@isep.ipp.pt>
2026-04-17 15:33:09 +0100

    Adicionar porta MySQL nas ligações PDO para Railway

--- private/processa_login.php
+++ private/processa_login.php
@@ -37,7 +37,7 @@ if (!empty($validation_errors)) {
 // --------------------------------------------------------------------
 try {
     $ligacao = new PDO(
-        "mysql:host=" . MYSQL_HOST . ";dbname=" . MYSQL_DATABASE . ";charset=utf8mb4",
+        "mysql:host=" . MYSQL_HOST . ";port=" . MYSQL_PORT . ";dbname=" . MYSQL_DATABASE . ";charset=utf8mb4",
         MYSQL_USERNAME,
         MYSQL_PASSWORD,
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
